a640c6b71ce300f86d74097cffbc24d719584440
Sheraz Akram <sherazakram.pro@gmail.com>
2023-10-13 20:36:23 +0500

    strings

---
+++ basics/strings.js
@@ -0,0 +1,34 @@
+const name = "sheraz";
+const repoCount = 26;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("sheraz-akram-pk");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "   sheraz    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sheraz.com/sheraz%20akram";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("ahmad"));
+
+console.log(gameName.split("-"));
