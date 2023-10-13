2e4e378f298a5c18716449eed13f8d2598d43316
Sheraz Akram <sherazakram.pro@gmail.com>
2023-10-13 18:11:25 +0500

    data types

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -2,16 +2,13 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 + 3); // code readability should be high
 
-console.log("Hitesh")
+console.log("Hitesh");
 
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
 
 // number => 2 to power 53
@@ -19,11 +16,46 @@ let state;
 // string => ""
 // boolean => true/false
 // null => standalone value
-// undefined => 
+// undefined =>
 // symbol => unique
-
 
 // object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn1 = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "hitesh",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
