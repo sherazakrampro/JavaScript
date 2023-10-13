cd21a4c329ebfed4890194fc882d74865aa6de45
Sheraz Akram <sherazakram.pro@gmail.com>
2023-10-13 19:15:34 +0500

    stack and heap

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -59,3 +59,32 @@ const myFunction = function () {
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Memory Types in JS
+// Stack (Primitive), Heap(Non-primitive)
+
+// Stack Example
+let myChannelName = "Learn With Sheraz";
+
+let anotherChannelName = myChannelName;
+
+anotherChannelName = "Learn With Ahmad";
+
+console.log(myChannelName);
+
+console.log(anotherChannelName);
+
+// Heap Example
+let userOne = {
+  name: "sheraz",
+  age: 36,
+};
+
+let userTwo = userOne;
+
+// this statement will change the name property of userOne also
+userTwo.name = "ahmad";
+
+console.log(userOne);
+
+console.log(userTwo);
